3254ae17699a31c4acc076ab6d5d8f8547a39367
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-06-30 16:58:05 +0700

    membuat fungsi void inorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -71,6 +71,20 @@ public:
 		}
 	}
 
+	void inorder(Node* ptr) 
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is emprty" << endl;
+			return;
+		}
+		if (ptr != NULL) 
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
 
 	}
 };
